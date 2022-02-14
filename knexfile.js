4ea8a91d20e5d3870fc9bba53b23327280c2cfa0
Tonipenyallop <kimtesu521@gmail.com>
2022-02-14 11:47:30 +0900

    fix config file

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
 
-  staging: {
+  production: {
     client: "postgresql",
     connection: {
       host: "ec2-34-198-31-223.compute-1.amazonaws.com",
@@ -22,22 +22,6 @@ module.exports = {
       user: "lwakluhfitwqzq",
       password:
         "311f7e4b184ee254398b956bf5da4c1520616b0a99c903cb00721059248f7929",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
     },
     pool: {
       min: 2,
